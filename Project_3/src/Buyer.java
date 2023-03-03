a5da31f6e49b590423ffc1d3338cf06620159065
Angel Romero <anro9968@colorado.edu>
2023-03-02 17:07:06 -0700

    Added Project 2 code(Revamped)

---
+++ Project_3/src/Buyer.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+// Creating the buyer class that will have a new name, preferred typing of vehicle.
+public class Buyer {
+    String name;
+    Enums.vehicleType preferredType;
+    private static int id = 0;
+    int buyingChance;
+
+    Random random = new Random();
+
+    public Buyer() {
+        // Increment values
+        name = "Buyer_"+id;
+        preferredType = Enums.vehicleType.values()[random.nextInt(3)];
+        buyingChance = random.nextInt(10);
+
+        // Instantiating their buying chance
+        if(buyingChance <= 6) {
+            buyingChance = 6; // 70% chance to buy 0 - 6
+        } else if (buyingChance <= 8) {
+            buyingChance = 2; // 20% chance to buy 7 - 8
+        } else {
+            buyingChance = 1; // 10% chance to buys
+        }
+
+        id++;
+    }
+
+    // Given the inventory of the cars, and the preferred vehicle type, will return the inventory with new values
+    public int buyCar(ArrayList<Vehicle> inventory) {
+        Vehicle car = new Vehicle();
+        int pos = 0;
+        // The above arrow is to save the car that it lands on
+
+        //Iterate through the vehicles and pick a car
+        for(int i = 0;i < inventory.size();i++) {
+            if(inventory.get(i).vType.equals(preferredType) && (!inventory.get(i).vCondition.equals(Enums.vehicleCondition.Broken)) && !inventory.get(i).sold) {
+                // save the car
+                car = inventory.get(i);
+                pos = i;
+                break;
+            }
+            // changed the buyers preference and restart the loop
+            if(i == inventory.size() - 1) {
+                preferredType = Enums.vehicleType.values()[random.nextInt(3)];
+                i=0;
+            }
+        }
+
+        if(car.vCondition.equals(Enums.vehicleCondition.New)) {
+            buyingChance++;
+        }
+
+        if(car.vCleanliness.equals(Enums.vehicleCleanliness.Sparkling)) {
+            buyingChance++;
+        }
+        return pos;
+    }
+}
---
+++ Project_3/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Angel Romero
+ * -- Revamped the code --
+ * 2/15/23
+ * Object-Oriented Analysis and Design
+ * Borrowed similar object design from the Project 2 code made by Bruce Montgomery
+ */
+
+// Executing the Simulator class(Executes the program)
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        // Running the Program, Run it!!!
+        Simulator sim = new Simulator();
+        sim.run();
+    }
+}
