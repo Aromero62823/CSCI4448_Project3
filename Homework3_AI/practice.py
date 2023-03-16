e27eec19f82d7c05f23dbe425041ac4d45da83a4
Angel Romero <anro9968@colorado.edu>
2023-03-15 19:58:33 -0600

    Readme update

---
+++ Homework3_AI/practice.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from collections import deque
+import heapq
+import unittest
+from scipy import stats
+import copy as cp
+from time import time
+###### All packages #######
+newli = list()
+target = [1, 2, 3, 4, 5, 6, 7, 8]
+for i in range(len(target)):
+    newli.append(target[i])
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# CSCI4448_Project3
-Working alone on this code.
+CSCI_4448 - Object-Oriented Analysis and Design
+Professor: Bruce Montgomery
+Date: 3/15/2023
+
+Members of this project: Angel Romero
+Notes:
+Borrowed Code from the Project2-FNDC zip file
+
